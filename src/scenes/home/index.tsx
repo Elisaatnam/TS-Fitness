57222f9bfd74211f11f46089530ee5b28f39536b
Elisaatnam <elisa.stueker@googlemail.com>
2024-10-01 15:52:45 +0200

    home done

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -36,7 +36,7 @@ const Home = ({ setSelectedPage }: Props) => {
                         </p>
                     </div>
                     {/* Actions */}
-                    <div>
+                    <div className="mt-8 flex items-center gap-8">
                         <ActionButton setSelectedPage={setSelectedPage}>
                             Join Now
                         </ActionButton>
@@ -53,15 +53,15 @@ const Home = ({ setSelectedPage }: Props) => {
                 </div>
 
                 {/* Image */}
-                <div>
+                <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
                     <img src={HomePageGraphic} alt="home-page-graphic" />
                 </div>
             </div>
             {/* Sponsors */}
             {isAboveMediumScreens && (
-                <div>
-                    <div>
-                        <div>
+                <div className="h-[150px] w-full bg-primary-100 py-10">
+                    <div className="mx-auto w-5/6">
+                        <div className="flex w-3/5 items-center justify-between gap-8">
                             <img src={SponsorRedBull} alt="red-bull-sponsor" />
                             <img src={SponsorForbes} alt="forbes-sponsor" />
                             <img src={SponsortFortune} alt="fortune-sponsor" />
