3ddcf293cb51d5f81a860b2bcb5ad4bb9c071039
Elisaatnam <elisa.stueker@googlemail.com>
2024-10-01 16:50:37 +0200

    Benefits WIP

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,33 @@
+import Heading from '@/shared/Heading'
+import { SelectedPage } from '@/shared/types'
+import {
+    HomeModernIcon,
+    UserGroupIcon,
+    AcademicCapIcon,
+} from '@heroicons/react/24/solid'
+import { motion } from 'framer-motion'
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void
+}
+
+const Benefits = ({ setSelectedPage }: Props) => {
+    return (
+        <section id="benefits" className="mx-auto min-h-full w-5/6 py-20 ">
+            <motion.div
+                onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+            >
+                <div>
+                    <Heading>More than just a gym</Heading>
+                    <p className="my-5 text-sm">
+                        We provide world class fitness equipment, trainers and
+                        classes to get you to your ultimate fitness goals with
+                        ease. We provide true care into each and every member.
+                    </p>
+                </div>
+            </motion.div>
+        </section>
+    )
+}
+
+export default Benefits
